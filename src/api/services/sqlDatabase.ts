031b73f3fe6121ef833aebcfb3a89a7233ea43b1
Agustin09810 <a.lorenzogon2@gmail.com>
2023-06-23 20:02:30 -0300

    Change of connector library

--- src/api/services/sqlDatabase.ts
+++ src/api/services/sqlDatabase.ts
@@ -1,17 +1,16 @@
-import { Client } from 'ts-postgres';
+import { Pool } from 'pg';
 import dotenv from "dotenv";
 
 dotenv.config();
 
 
-
-const client = new Client({
+export const client = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    //connectionLimit: 10
-});
+    port: 5432
+})
 
 client.connect();
 
